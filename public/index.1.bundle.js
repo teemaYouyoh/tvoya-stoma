f24b36c91a62c1d0fc5a6c95caa69c19cf8665d2
Timofey <tyunin.timofey2001@gmail.com>
2023-12-14 10:48:51 +0200

    commit for merge

--- public/index.1.bundle.js
+++ public/index.1.bundle.js
@@ -1,1 +1,16 @@
-"use strict";(self.webpackChunkpersona_patman=self.webpackChunkpersona_patman||[]).push([[873],{},a=>{a.O(0,[216,607],(()=>(607,a(a.s=607)))),a.O()}]);
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkpersona_patman"] = self["webpackChunkpersona_patman"] || []).push([["index.1"],{},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ __webpack_require__.O(0, ["vendors","src_index_ts"], () => (__webpack_exec__("./src/index.ts")));
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ }
+]);
